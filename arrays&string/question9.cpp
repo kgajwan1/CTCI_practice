84902411cbd4af8fa7b71a7ef09b74004e80ab39
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2019-06-29 23:13:20 -0400

    Create question9.cpp
    Question 9, CTCI

---
+++ arrays&string/question9.cpp
@@ -0,0 +1,26 @@
+//question 9 CTCI arrays & strings
+#include<bits/stdc++.h>
+//is s2 a rotation of s1?
+//best way is to check if s2 is subset if s1s1
+
+
+bool isRotation(std::string s1, std::string s2){
+    int len = s1.length();
+    /*check if length of s1 and s2 are same or not */
+    if(len = s2.length() && len >0){
+        std::string s1s1 = s1 + s1;
+        if ( s1s1.find(s2) != std::string::npos ) {
+		return true;
+	} else {
+  	return false;
+}
+    }
+    return false;
+}
+
+int main(){
+    std::string s1 ="hello";
+    std::string s2 ="lohel";
+    std::cout <<std::boolalpha<<isRotation(s2,s1);
+return 0;
+}
