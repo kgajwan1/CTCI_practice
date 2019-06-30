734bc9ec04adc836345076ed1b714c61793ce413
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2019-06-29 23:14:10 -0400

    Update question9.cpp

--- arrays&string/question9.cpp
+++ arrays&string/question9.cpp
@@ -3,17 +3,14 @@
 //is s2 a rotation of s1?
 //best way is to check if s2 is subset if s1s1
 
-
 bool isRotation(std::string s1, std::string s2){
     int len = s1.length();
-    /*check if length of s1 and s2 are same or not */
+    /*check if length of s1 and s2 aer same or not */
     if(len = s2.length() && len >0){
         std::string s1s1 = s1 + s1;
         if ( s1s1.find(s2) != std::string::npos ) {
 		return true;
-	} else {
-  	return false;
-}
+	} 
     }
     return false;
 }
